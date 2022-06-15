2000f74a166d57a30545cd7b20430a0b62ad12ef
Smokey <hnkmbo@gmail.com>
2022-06-15 10:41:56 -0500

    spes: ScarlettAOSP bringup

--- scarlett_spes.mk
+++ scarlett_spes.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2022 The LineageOS Project
+# Copyright (C) 2022 The ScarlettAOSP Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -11,18 +11,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from spes/spesn device
 $(call inherit-product, device/xiaomi/spes/device.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common ScarlettAOSP stuff.
+$(call inherit-product, vendor/scarlett/config/common_full_phone.mk)
 
-# Additions
-$(call inherit-product-if-exists, vendor/gapps/common.mk)
-$(call inherit-product-if-exists, vendor/prebuilt-apps/config.mk)
-
-# Camera
-$(call inherit-product-if-exists, vendor/miuicamera/config.mk)
+# Boot animation
+TARGET_BOOT_ANIMATION_RES := 1080
 
 # Product Specifics
-PRODUCT_NAME := lineage_spes
+PRODUCT_NAME := scarlett_spes
 PRODUCT_DEVICE := spes
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 11
@@ -31,11 +27,7 @@ PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 # Overlay
-DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
-
-# LiveDisplay
-PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service-sdm
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-scarlett
 
 # Fingerprint
 BUILD_FINGERPRINT := "Redmi/spes/spes:12/SKQ1.211103.001/V13.0.6.0.SGKMIXM:user/release-keys"
